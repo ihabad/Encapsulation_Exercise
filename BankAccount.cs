eb0a350a10f1e386e985ad38edea78b991981a09
ihababoudiab@gmail.com <ihababoudiab@gmail.com>
2022-08-18 11:08:48 -0700

    Encapsulation Exercise

---
+++ BankAccount.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulation
+{
+    internal class BankAccount
+    {
+         public BankAccount()
+            {
+            }
+
+        private double balance = 0;
+        
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        public double GetBalance()
+        { 
+          return balance;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Dynamic;
+
+namespace BankEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            Console.WriteLine("What amount would you like to deposit today?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Thank you");
+
+            account.Deposit(amountToDeposit);
+            Console.WriteLine($"Your total bank account balance is now! {account.GetBalance()}$");
+        }
+    }
+}
